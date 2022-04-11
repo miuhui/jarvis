9104151614a4e5ffbbad3cb92c6d9e8b3636ff64
miaohui <miaohui821@gmail.com>
2022-04-12 00:01:22 +0800

    [ADD]log

--- src/main/java/com/tsa/jarvis/entity/UserDO.java
+++ src/main/java/com/tsa/jarvis/entity/UserDO.java
@@ -13,13 +13,21 @@ public class UserDO {
   @TableId(type = IdType.INPUT)
   private String id;
 
+  /** 用户id */
   private String userId;
 
+  /** 用户姓名 */
   private String userName;
 
+  /** 性别 */
+  private String gender;
+
+  /** 密码 */
   private String password;
 
+  /** 创建时间 */
   private Date gmtCreated;
 
+  /** 修改时间 */
   private Date gmtModified;
 }
--- src/main/java/com/tsa/jarvis/util/DateUtil.java
+++ src/main/java/com/tsa/jarvis/util/DateUtil.java
@@ -12,6 +12,12 @@ public class DateUtil {
 
   public static final String SHORT_DATE_PATTERN = "yyyyMMdd";
 
+  /**
+   * 将日期转化为yyyyMMdd格式的字符串
+   *
+   * @param d 代转化日期
+   * @return yyyyMMdd格式的字符串
+   */
   public static String shortDateStr(LocalDate d) {
     return DateTimeFormatter.ofPattern(SHORT_DATE_PATTERN).format(d);
   }

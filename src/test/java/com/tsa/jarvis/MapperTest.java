59a35db7e974aea3973f17a6b0918495594052d3
Samuel <miaohui821@hotmail.com>
2022-05-29 00:21:26 +0800

    [feat]remove unnecessary import

--- src/test/java/com/tsa/jarvis/MapperTest.java
+++ src/test/java/com/tsa/jarvis/MapperTest.java
@@ -1,8 +1,6 @@
 package com.tsa.jarvis;
 
-import com.tsa.jarvis.entity.UserDO;
 import com.tsa.jarvis.mapper.UserMapper;
-import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
